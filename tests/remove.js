05af5d879720fc1908a38b5c141ec1e72e26bd9b
Matt Bierner <mattbierner@gmail.com>
2016-01-02 17:51:05 -0800

    Moving more remove tests to mocha

--- tests/remove.js
+++ tests/remove.js
@@ -1,102 +1,119 @@
-var hamt = require('../dist_node/hamt');
+"use strict";
+const hamt = require('../dist_node/hamt');
+const assert = require('chai').assert;
 
-
-exports.single = function(test) {
-    var h = hamt.set('a', 3, hamt.empty);
-    test.equal(hamt.get('a', h), 3);
+describe('remove', () => {
+    it('should noop on empty', () => {
+        assert.strictEqual(0, hamt.count(hamt.remove('a', hamt.empty)));
+        assert.strictEqual(0, hamt.count(hamt.remove('b', hamt.empty)));
+    });
     
-    var h1 = hamt.remove('a', h);
-    test.equal(hamt.get('a', h1), null);
+    it('should remove value from single map', () => {
+        const h1 = hamt.set('a', 3, hamt.empty);
+        const h2 = hamt.remove('a', h1);
+        
+        assert.strictEqual(0, hamt.count(h2));
+        assert.strictEqual(null, hamt.get('a', h2));
+        
+        assert.strictEqual(1, hamt.count(h1));
+        assert.strictEqual(3, hamt.get('a', h1));
+    });
+    
+    it('should only remove a single entry', () => {
+        const h1 = hamt.set('b', 5, hamt.set('a', 3, hamt.empty));
+        const h2 = hamt.remove('a', h1);
+        
+        assert.strictEqual(1, hamt.count(h2));
+        assert.strictEqual(null, hamt.get('a', h2));
+        assert.strictEqual(5, hamt.get('b', h2));
 
-    test.done();
-};
-
-exports.remove_on_empty = function(test) {
-    var h = hamt.remove('x', hamt.empty);
-    test.done();
-};
-
+        assert.strictEqual(2, hamt.count(h1));
+        assert.strictEqual(3, hamt.get('a', h1));
+        assert.strictEqual(5, hamt.get('b', h1));
+    });
+});
+   
 exports.delete_one_entry = function(test) {
-    var h1 = hamt.set('b', 5, hamt.set('a', 3, hamt.empty));
+    const h1 = hamt.set('b', 5, hamt.set('a', 3, hamt.empty));
     
-    var h2 = hamt.remove('a', h1);
-    test.equal(hamt.get('a', h2), null);
-    test.equal(hamt.get('b', h2), 5);
+    const h2 = hamt.remove('a', h1);
+    assert.strictEqual(hamt.get('a', h2), null);
+    assert.strictEqual(hamt.get('b', h2), 5);
     
-    var h3 = hamt.remove('b', h2);
-    test.equal(hamt.get('a', h3), null);
-    test.equal(hamt.get('b', h3), null);
+    const h3 = hamt.remove('b', h2);
+    assert.strictEqual(hamt.get('a', h3), null);
+    assert.strictEqual(hamt.get('b', h3), null);
     
-    test.done();
+    assert.done();
 };
 
 exports.remove_does_not_alter_original = function(test) {
-    var h1 = hamt.set('b', 5,hamt.set('a', 3, hamt.empty));
+    const h1 = hamt.set('b', 5,hamt.set('a', 3, hamt.empty));
     
-    var h2 = hamt.remove('a', h1);
+    const h2 = hamt.remove('a', h1);
     
-    test.equal(hamt.get('a', h1), 3);
-    test.equal(hamt.get('b', h1), 5);
+    assert.strictEqual(hamt.get('a', h1), 3);
+    assert.strictEqual(hamt.get('b', h1), 5);
     
-    test.equal(hamt.get('a', h2), null);
-    test.equal(hamt.get('b', h2), 5);
+    assert.strictEqual(hamt.get('a', h2), null);
+    assert.strictEqual(hamt.get('b', h2), 5);
     
-    test.done();
+    assert.done();
 };
 
 exports.delete_collision = function(test) {
-    var h1 = hamt.setHash(0, 'b', 5, hamt.setHash(0, 'a', 3, hamt.empty));
-    var h2 = hamt.removeHash(0, 'a', h1);
+    const h1 = hamt.setHash(0, 'b', 5, hamt.setHash(0, 'a', 3, hamt.empty));
+    const h2 = hamt.removeHash(0, 'a', h1);
     
-    test.equal(hamt.getHash(0, 'a', h2), null);
-    test.equal(hamt.getHash(0, 'b', h2), 5);
+    assert.strictEqual(hamt.getHash(0, 'a', h2), null);
+    assert.strictEqual(hamt.getHash(0, 'b', h2), 5);
     
-    test.done();
+    assert.done();
 };
 
 exports.delete_collision_no_exists = function(test) {
-    var h1 = hamt.setHash(0, 'b', 5, hamt.setHash(0, 'a', 3, hamt.empty));
-    var h2 = hamt.removeHash(0, 'c', h1);
+    const h1 = hamt.setHash(0, 'b', 5, hamt.setHash(0, 'a', 3, hamt.empty));
+    const h2 = hamt.removeHash(0, 'c', h1);
     
-    test.equal(hamt.getHash(0, 'a', h2), 3);
-    test.equal(hamt.getHash(0, 'b', h2), 5);
-    test.equal(hamt.getHash(0, 'c', h2), null);
+    assert.strictEqual(hamt.getHash(0, 'a', h2), 3);
+    assert.strictEqual(hamt.getHash(0, 'b', h2), 5);
+    assert.strictEqual(hamt.getHash(0, 'c', h2), null);
 
-    test.done();
+    assert.done();
 };
 
 exports.remove_many = function(test) {
-    var insert = ["n", "U", "p", "^", "h", "w", "W", "x", "S", "f", "H", "m", "g",
+    const insert = ["n", "U", "p", "^", "h", "w", "W", "x", "S", "f", "H", "m", "g",
                "l", "b", "_", "V", "Z", "G", "o", "F", "Q", "a", "k", "j", "r",
                "B", "A", "y", "\\", "R", "D", "i", "c", "]", "C", "[", "e", "s",
                "t", "J", "E", "q", "v", "M", "T", "N", "L", "K", "Y", "d", "P",
                "u", "I", "O", "`", "X"];
     
-    var remove = ["w", "m", "Q", "R", "i", "K", "P", "Y", "D", "g", "y", "L",
+    const remove = ["w", "m", "Q", "R", "i", "K", "P", "Y", "D", "g", "y", "L",
                   "b", "[", "a", "t", "j", "W", "J", "G", "q", "r", "p", "U",
                   "v", "h", "S", "_", "d", "x", "I", "F", "f", "n", "B", "\\",
                   "k", "V", "N", "l", "X", "A", "]", "s", "Z", "O", "^", "o",
                   "`", "H", "E", "e", "M", "u", "T", "c", "C"];
     
-    var h = hamt.empty;
+    const h = hamt.empty;
     insert.forEach(function(x) {
         h = hamt.set(x, x, h);
     });
     
-    for (var i = 0; i < remove.length; ++i) {
+    for (const i = 0; i < remove.length; ++i) {
         h = hamt.remove(remove[i], h);
         
-        for (var g = 0; g <= i; ++g) {
-            test.equal(
+        for (const g = 0; g <= i; ++g) {
+            assert.strictEqual(
                 hamt.get(remove[g], h),
                 null);
         }
-        for (var g = i + 1; g < remove.length; ++g) {
-            test.equal(
+        for (const g = i + 1; g < remove.length; ++g) {
+            assert.strictEqual(
                 hamt.get(remove[g], h),
                 remove[g]);
         }
     }
 
-    test.done();
+    assert.done();
 };
