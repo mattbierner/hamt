d54363c6377d4719d79c5b5ae5ce059b46fa6065
Matt Bierner <mattbierner@gmail.com>
2016-01-02 17:45:42 -0800

    Switch values test to use mocha

--- tests/values.js
+++ tests/values.js
@@ -1,55 +1,36 @@
-var hamt = require('../dist_node/hamt');
+"use strict";
+const hamt = require('../dist_node/hamt');
+const assert = require('chai').assert;
 
-var containsAll = function(test, arr, keys) {
-    keys.forEach(function(k) {
-        test.ok(arr.indexOf(k) >= 0, k);
-    });
-};
-
-
-exports.empty = function(test) {
-    test.deepEqual(
-        hamt.values(hamt.empty),
-        []);
-
-    test.done();
-};
-
-exports.simple_keys= function(test) {
-    var h1 = hamt.set('b', 5, hamt.set('a', 3, hamt.empty));
-    
-    containsAll(test,
-        hamt.values(h1),
-        [5, 3]);
-
-    test.done();
-};
-
-exports.collision = function(test) {
-    var h1 = hamt.setHash(0, 'b', 5, hamt.setHash(0, 'a', 3, hamt.empty));
-        
-    containsAll(test,
-        hamt.values(h1),
-        [5, 3]);
-    
-    test.done();
-};
-
-exports.many = function(test) {
-    var insert = ["n", "U", "p", "^", "h", "w", "W", "x", "S", "f", "H", "m", "g",
-               "l", "b", "_", "V", "Z", "G", "o", "F", "Q", "a", "k", "j", "r",
-               "B", "A", "y", "\\", "R", "D", "i", "c", "]", "C", "[", "e", "s",
-               "t", "J", "E", "q", "v", "M", "T", "N", "L", "K", "Y", "d", "P",
-               "u", "I", "O", "`", "X"];
-    
-    var h = hamt.empty;
-    insert.forEach(function(x) {
-        h = hamt.set(x, x, h);
+describe('values', () => {
+    it('should return empty for empty map', () => {
+        assert.deepEqual([], hamt.values(hamt.empty));
     });
     
-    containsAll(test,
-        hamt.values(h),
-        insert);
+    it('should return single key for single element map', () => {
+        assert.deepEqual([3], hamt.values(hamt.set('a', 3, hamt.empty)));
+        assert.deepEqual([5], hamt.values(hamt.set('b', 5, hamt.empty)));
+    });
     
-    test.done();
-};
+    it('should return all values for collision', () => {
+        const h1 = hamt.setHash(0, 'b', 5, hamt.setHash(0, 'a', 3, hamt.empty));
+        assert.sameMembers([5, 3], hamt.values(h1));
+    });
+    
+    it('return correct values while items are added', () => {
+        const insert = [
+            "n", "U", "p", "^", "h", "w", "W", "x", "S", "f", "H", "m", "g",
+            "l", "b", "_", "V", "Z", "G", "o", "F", "Q", "a", "k", "j", "r",
+            "B", "A", "y", "\\", "R", "D", "i", "c", "]", "C", "[", "e", "s",
+            "t", "J", "E", "q", "v", "M", "T", "N", "L", "K", "Y", "d", "P",
+            "u", "I", "O", "`", "X"];
+    
+        let h = hamt.empty;
+        insert.forEach(x => {
+            h = hamt.set(x, x, h);
+        });
+    
+        assert.sameMembers(insert, hamt.values(h));
+    });
+});
+
