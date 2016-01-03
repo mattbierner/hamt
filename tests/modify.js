1bd2aeea2c9c727b5105ac04f70e0187f3720f79
Matt Bierner <mattbierner@gmail.com>
2016-01-02 17:56:53 -0800

    ported over modify tests

--- tests/modify.js
+++ tests/modify.js
@@ -1,40 +1,36 @@
-var hamt = require('../dist_node/hamt');
+"use strict";
+const hamt = require('../dist_node/hamt');
+const assert = require('chai').assert;
 
+describe('modify', () => {
+    it('should update entry in single map', () => {
+        const h = hamt.set('a', 3, hamt.empty);
+        const h1 = hamt.modify('a', x => x * 2, h);
 
-exports.single = function(test) {
-    var h = hamt.set('a', 3, hamt.empty);
-    var h1 = hamt.modify('a', function(x) { return x * 2 }, h);
+        assert.strictEqual(hamt.get('a', h1), 6);
+    });
 
-    test.equal(hamt.get('a', h1), 6);
+    it('should insert into empty map', () => {
+        const h = hamt.modify('a', function(x) { return 10; }, hamt.empty);
+        assert.strictEqual(hamt.get('a', h), 10);
+    });
+
+    it('should modify collision values correctly', () => {
+        const h1 = hamt.setHash(0, 'a', 3, hamt.empty);
+        const h2 = hamt.setHash(0, 'b', 5, h1);
     
-    test.done();
-};
-
-exports.non_existant = function(test) {
-    var h = hamt.modify('a', function(x) { return 10; }, hamt.empty);
-    test.equal(hamt.get('a', h), 10);
+        const h3 = hamt.modifyHash(0, 'a', function(x) { return x * 2; }, h2);
+        assert.strictEqual(hamt.getHash(0, 'a', h3), 6);
+        assert.strictEqual(hamt.getHash(0, 'b', h3), 5);
     
-    test.done();
-};
-
-exports.collision = function(test) {
-    var h1 = hamt.setHash(0, 'a', 3, hamt.empty);
-    var h2 = hamt.setHash(0, 'b', 5, h1);
+        const h4 = hamt.modifyHash(0, 'b', function(x) { return x * 2; }, h3);
+        assert.strictEqual(hamt.getHash(0, 'a', h4), 6);
+        assert.strictEqual(hamt.getHash(0, 'b', h4), 10);
     
-    var h3 = hamt.modifyHash(0, 'a', function(x) { return x * 2; }, h2);
-    test.equal(hamt.getHash(0, 'a', h3), 6);
-    test.equal(hamt.getHash(0, 'b', h3), 5);
-    
-    var h4 = hamt.modifyHash(0, 'b', function(x) { return x * 2; }, h3);
-    test.equal(hamt.getHash(0, 'a', h4), 6);
-    test.equal(hamt.getHash(0, 'b', h4), 10);
-    
-    // Non existant
-    var h5 = hamt.modifyHash(0, 'c', function(x) { return 100; }, h4);
-    test.equal(hamt.getHash(0, 'a', h5), 6);
-    test.equal(hamt.getHash(0, 'b', h5), 10);
-    test.equal(hamt.getHash(0, 'c', h5), 100);
-
-    test.done();
-};
-
+        // Non existant
+        const h5 = hamt.modifyHash(0, 'c', function(x) { return 100; }, h4);
+        assert.strictEqual(hamt.getHash(0, 'a', h5), 6);
+        assert.strictEqual(hamt.getHash(0, 'b', h5), 10);
+        assert.strictEqual(hamt.getHash(0, 'c', h5), 100);
+    });
+});
